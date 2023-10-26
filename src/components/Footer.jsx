270933a2687007bc40abef7337bc3a31c1e244a3
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-10-25 22:28:06 -0400

    adds components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+return (Footer = () => {
+  return (
+    <header>
+      <nav></nav>
+    </header>
+  );
+});
---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import NavTabs from "./NavTabs";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+import Resume from "./pages/Resume";
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      <main className="mx-3">{renderPage()}</main>
+    </div>
+  );
+}
